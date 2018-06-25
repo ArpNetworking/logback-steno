8289575eabd68f3729cb352fb2afd2b49e233b11
Ville Koskela <ville@koskelafamily.com>
2018-06-25 15:46:08 -0700

    Fix copyright headers. (#59)

--- src/main/java/com/arpnetworking/logback/ProcessConverter.java
+++ src/main/java/com/arpnetworking/logback/ProcessConverter.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright 2014 Groupon.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
--- src/test/java/com/arpnetworking/StenoEncoderExceptionIntegrationTest.java
+++ src/test/java/com/arpnetworking/StenoEncoderExceptionIntegrationTest.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright 2016 Ville Koskela
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
